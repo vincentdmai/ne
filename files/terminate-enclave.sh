1e8c320b297d9f4053d09ad5d7d516936b399324
Bryant Nguyen <bnguyen@WHITEBOARDFEDERAL.LCL>
2023-03-29 14:15:24 -0400

    Remove docker images during enclave termination

--- files/terminate-enclave.sh
+++ files/terminate-enclave.sh
@@ -2,3 +2,4 @@
 
 ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
 [ "$ENCLAVE_ID" != "null" ] && nitro-cli terminate-enclave --enclave-id ${ENCLAVE_ID}
+docker rmi -f $(docker images -a -q)
